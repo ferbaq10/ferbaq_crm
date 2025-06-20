b1744bf503c887056ae8a506d1b22b53f483ef55
Ricardo Villanueva <ricardo.villanueva@ferbaq.com.mx>
2025-06-20 12:13:45 -0600

    fix(viewset.py) agregada get_serializer_class al viewset d eoportunity

--- opportunity/viewsets.py
+++ opportunity/viewsets.py
@@ -1,11 +1,18 @@
 from catalog.viewsets import AuthenticatedModelViewSet
 from .models import Opportunity, CommercialActivity
-from .serializers import OpportunitySerializer, CommercialActivitySerializer
+from .serializers import OpportunitySerializer, CommercialActivitySerializer, OpportunityWriteSerializer
 
 
 class OpportunityViewSet(AuthenticatedModelViewSet):
     model = Opportunity
     serializer_class = OpportunitySerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:  # Para GET (lista o detalle)
+            return OpportunitySerializer
+        return OpportunityWriteSerializer
+
+
 
 
 class CommercialActivityViewSet(AuthenticatedModelViewSet):
